70a75a9ed055a0dac39f512af837f67792801b3b
Marco Bertonati <marko.bertonati@gmail.com>
2021-04-24 21:51:25 -0300

    =le agregué alt a las imágenes y me sale como que están rotas

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -2,25 +2,25 @@ import React from "react";
 import "./ItemDetail.css";
 
 export default function ShowItemDetail({ arrayProducts }) {
-  
   console.log(arrayProducts);
-
 
   return (
     <div className="itemDetail">
       <h3 className="title-h3">DESCRIPCIÓN DE ITEMS</h3>
 
-      {arrayProducts.map((eachProduct) => (
-        <div className="eachItemDetail" key={eachProduct.id}>
-          <div className="ItemDetail-img">
-            <img scr={eachProduct.photo} />
+      {arrayProducts.map((eachProduct) => {
+        return (
+          <div className="eachItemDetail" key={eachProduct.id}>
+
+              <img scr= {eachProduct.photo} alt={eachProduct.title}></img>
+
+            <ul>
+              <li>{eachProduct.description}</li>
+              <li>¿Ta caro no? {eachProduct.price}</li>
+            </ul>
           </div>
-          <ul>
-            <li>{eachProduct.description}</li>
-            <li>¿Ta caro no? {eachProduct.price}</li>
-          </ul>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
