0ccf4a850de72aae4dfe1ffcf7af9efdfc32adb2
Marco Bertonati <marko.bertonati@gmail.com>
2021-04-24 21:34:37 -0300

    =se ven las imágenes en las card pero no en la descripción

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,17 +1,16 @@
 import React from "react";
 import {Card, Button} from "react-bootstrap";
-import ImgRing from '../../Assets/Img/Ring.jpg'
 import ItemCountContainer from '../../Container/ItemCountContainer'
 
-export default function showItem({title}) {
+export default function showItem({title, image}) {
   return (
     <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={ImgRing} />
+      <Card.Img variant="top" src={image} />
       <Card.Body>
-        <Card.Title>Nombre del Producto: {title}</Card.Title>
-        <Card.Text>
+        <Card.Title>{title}</Card.Title>
+        {/* <Card.Text>
           Esta sería la descripción de todo el producto.
-        </Card.Text>
+        </Card.Text> */}
         <Button variant="primary">Detalles del Producto</Button>
         <ItemCountContainer/>
       </Card.Body>
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -2,23 +2,25 @@ import React from "react";
 import "./ItemDetail.css";
 
 export default function ShowItemDetail({ arrayProducts }) {
+  
   console.log(arrayProducts);
+
 
   return (
     <div className="itemDetail">
-
       <h3 className="title-h3">DESCRIPCIÓN DE ITEMS</h3>
 
       {arrayProducts.map((eachProduct) => (
         <div className="eachItemDetail" key={eachProduct.id}>
-            <img className="ItemDetail-img" scr={eachProduct.photo}/>
+          <div className="ItemDetail-img">
+            <img scr={eachProduct.photo} />
+          </div>
           <ul>
-              <li>{eachProduct.description}</li>
-              <li>¿Ta caro no? {eachProduct.price}</li>
+            <li>{eachProduct.description}</li>
+            <li>¿Ta caro no? {eachProduct.price}</li>
           </ul>
         </div>
       ))}
-
     </div>
   );
 }
