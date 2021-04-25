7604ca8853ebdb49c250c539d50cdfa6fe8669df
Marco Bertonati <marko.bertonati@gmail.com>
2021-04-24 21:05:13 -0300

    =Funciona todo pero no se me ven las imágenes - Desafio 6

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -6,33 +6,19 @@ export default function ShowItemDetail({ arrayProducts }) {
 
   return (
     <div className="itemDetail">
+
       <h3 className="title-h3">DESCRIPCIÓN DE ITEMS</h3>
 
+      {arrayProducts.map((eachProduct) => (
+        <div className="eachItemDetail" key={eachProduct.id}>
+            <img className="ItemDetail-img" scr={eachProduct.photo}/>
+          <ul>
+              <li>{eachProduct.description}</li>
+              <li>¿Ta caro no? {eachProduct.price}</li>
+          </ul>
+        </div>
+      ))}
 
-      {
-
-        arrayProducts.map(eachProduct=>{
-            
-            <div>
-                {eachProduct.description}
-            </div>
-        })
-
-      }
-
-
-
-      {/* {
-      
-      arrayProducts.map(eachItem => {
-        <div className="colorP" key={eachItem.id}>
-          <p>{eachItem.description}</p>
-          <p>{eachItem.price}</p>
-          <img scr={eachItem.photo} />
-        </div>;
-      })
-
-      } */}
     </div>
   );
 }
